2e8fee950877cb47d05ac4e80ab15bf487828d27
nevada_scout <travis@impactmod.com>
2022-01-17 15:07:20 +0100

    Redirect to github

--- api/src/App/Controllers/OAuthController.php
+++ api/src/App/Controllers/OAuthController.php
@@ -6,6 +6,13 @@ class OAuthController extends BaseController
 {
     public function goToGithub()
     {
+        // Save the state for comparison later
+        $_SESSION["GITHUB_AUTH_STATE"] = $this->generateState();
+
+        $redirectUrl = "https://github.com/login/oauth/authorize?client_id={$_ENV["GITHUB_CLIENT_ID"]}&state={$_SESSION["GITHUB_AUTH_STATE"]}&redirect_uri={$_ENV["OAUTH_REDIRECT_URI"]}";
+
+        header("Location: {$redirectUrl}");
+        exit();
     }
 
     public function callback()
