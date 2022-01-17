96bb6360dc04092db0ccc1460d61e71080de61da
nevada_scout <travis@impactmod.com>
2022-01-17 15:07:32 +0100

    Helper functions for oauth callback

--- api/src/App/Controllers/OAuthController.php
+++ api/src/App/Controllers/OAuthController.php
@@ -17,5 +17,30 @@ class OAuthController extends BaseController
 
     public function callback()
     {
+
+    private function generateState()
+    {
+        $factory = new \RandomLib\Factory;
+        $generator = $factory->getMediumStrengthGenerator();
+
+        // Generate a random string to use
+        return $generator->generateString(
+            25,
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+        );
+    }
+
+    private function httpPost($url, $data)
+    {
+        $curl = curl_init($url);
+
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return $response;
     }
 }
